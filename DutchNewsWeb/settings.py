c05727445359938f034acc25c3a2a564cf38258a
AnaAvakimova <nastassiavakimova@gmail.com>
2023-12-20 18:10:17 +0100

    pagination added

--- DutchNewsWeb/settings.py
+++ DutchNewsWeb/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^*2+ldie!lyztus5esks3jb_0x((pfran-6z01q^00x_4341gn'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-^*2+ldie!lyztus5esks3jb_0x((pfran-6z01q^00x_4341gn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -110,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
--- news/views.py
+++ news/views.py
@@ -1,44 +1,27 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import News
+from django.http import JsonResponse
 
 
 def get_news(request):
-    articles = News.objects.all()
-    articles = list(reversed(articles))
-    context = {
-         'articles': articles
-    }
-    return render(request, 'news.html', context)
-
-    # paginator = Paginator(articles, 5)  # Show 5 articles per page.
-    # page_number = request.GET.get("page")
-    # page_obj = paginator.get_page(page_number)
-
-    # first_articles, second_articles, third_articles, forth_articles, fifth_articles = [], [], [], [], []
-    # for index, article in enumerate(articles):
-    #     if index == 0:  # For the very first article
-    #         first_articles.append(article)
-    #     # elif index % 5 == 0:
-    #     #     second_articles.append(article)
-    #     elif index % 4 == 1:
-    #         second_articles.append(article)
-    #     elif index % 4 == 2:
-    #         third_articles.append(article)
-    #     elif index % 4 == 3:
-    #         forth_articles.append(article)
-    #     elif index % 4 == 0:
-    #         fifth_articles.append(article)
-    #
+    # articles = News.objects.all()
+    # articles = list(reversed(articles))
     # context = {
-    #     'first_articles': first_articles,
-    #     'second_articles': second_articles,
-    #     'third_articles': third_articles,
-    #     'forth_articles': forth_articles,
-    #     'fifth_articles': fifth_articles
-    #     # 'page_obj': page_obj
+    #     'articles': articles
     # }
     # return render(request, 'news.html', context)
+
+    articles = News.objects.all().order_by('-created_at')
+    paginator = Paginator(articles, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'articles': articles,
+        'page_obj': page_obj
+    }
+    return render(request, 'news.html', context)
 
 
 def get_article(request, num):
