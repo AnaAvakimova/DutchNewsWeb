59a9291eb32c56f49b102a3a6a21b8247a1099cb
AnaAvakimova <nastassiavakimova@gmail.com>
2023-12-30 14:40:35 +0100

    fixed bug in database settings

--- DutchNewsWeb/settings/base.py
+++ DutchNewsWeb/settings/base.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'DutchNewsWeb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db/db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
