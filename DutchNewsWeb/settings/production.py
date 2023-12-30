4be16c835bef0e869ee47ac81415fbead6cd18fb
AnaAvakimova <nastassiavakimova@gmail.com>
2023-12-30 15:03:19 +0100

    settings changed

--- DutchNewsWeb/settings/production.py
+++ DutchNewsWeb/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *
 
-DEBUG = True
-ALLOWED_HOSTS = ['89.116.243.80', 'localhost', '127.0.0.1', 'your_server_ip']
+DEBUG = False
+ALLOWED_HOSTS = ['89.116.243.80']
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
