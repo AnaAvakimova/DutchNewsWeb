c199b41cc56e6c868c91f95928714d89b62e933f
AnaAvakimova <nastassiavakimova@gmail.com>
2023-12-30 20:14:03 +0100

    static files&settings changed

--- DutchNewsWeb/settings/production.py
+++ DutchNewsWeb/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['89.116.243.80']
+ALLOWED_HOSTS = ['89.116.243.80', 'sketch.news', 'www.sketch.news']
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
