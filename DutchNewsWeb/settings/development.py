fff4da43cf5890355cafce82cb9de2f63b12d74f
AnaAvakimova <nastassiavakimova@gmail.com>
2024-01-12 12:57:35 +0100

    docker-compose added;css styles changed

--- DutchNewsWeb/settings/development.py
+++ DutchNewsWeb/settings/development.py
@@ -2,4 +2,5 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
